95ba7af36a8621fccfd2ee9c0ad6ed86953a57ac
Pedro <pedro@42mate.com>
2021-10-24 23:28:28 -0300

    Start restcountries challange

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const https = require("https");
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+let holeInfo = [];
+let str = "";
+let body = "";
+const url = "https://restcountries.com/v2/all";
+app.get("/", (req, res) => {
+  https.get(url, function (response) {
+    response.on("data", (chunk) => (body += chunk));
+    response.on("end", () => {
+      holeInfo = JSON.parse(body);
+      res.render("allCountries", {
+          countries: holeInfo
+      });
+      body = "";
+    });
+  });
+});
+
+
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
+
+// let body = [];
+// request.on('data', (chunk) => {
+//   body.push(chunk);
+// }).on('end', () => {
+//   body = Buffer.concat(body).toString();
+//   // at this point, `body` has the entire request body stored in it as a string
+// });
