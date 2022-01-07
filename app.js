6a7bb77f9957291bd813dd9b30633c1d8fe9e1f7
Pedro <pedroalegre29@gmail.com>
2022-01-06 23:44:32 -0300

    Continue coding

--- app.js
+++ app.js
@@ -10,14 +10,14 @@ app.use(express.static("public"));
 let holeInfo = [];
 let str = "";
 let body = "";
-const url = "https://restcountries.com/v2/all";
+let url = "https://restcountries.com/v3.1/all";
 
 app.get("/", (req, res) => {
   https.get(url, function (response) {
     response.on("data", (chunk) => (body += chunk));
     response.on("end", () => {
       holeInfo = JSON.parse(body);
-      res.render("allCountries", {
+      res.render("countriesList", {
         countries: holeInfo,
       });
       body = "";
@@ -25,63 +25,90 @@ app.get("/", (req, res) => {
   });
 });
 
-// app.get("/:country", (req, res) => {
-//   const singleCountryUrl =
-//     "https://restcountries.com/v2/name/" + req.params.country;
-//   https.get(singleCountryUrl, function (response) {
-//     response.on("data", function (data) {
-//       const countryData = JSON.parse(data);
+app.get("/:country", (req, res) => {
+  if (req.params.country) {
+    const singleCountryUrl =
+      "https://restcountries.com/v3.1/name/" + req.params.country;
+    https.get(singleCountryUrl, function (response) {
+      response.on("data", function (data) {
+        const countryData = JSON.parse(data);
 
-//       var languages = " ";
+        var languages = " ";
 
-//       if (countryData[0].languages.length === 1) {
-//         languages = languages + countryData[0].languages[0].name;
-//       } else {
-//         var index = 0;
-//         for (index; index < (countryData[0].languages.length) - 1; index++) {
-//           languages = languages + countryData[0].languages[index].name + ", ";
-//         }
-//         languages = languages + countryData[0].languages[index].name;
-//       }
+        if (countryData[0].languages.length === 1) {
+          languages = languages + countryData[0].languages[0].name;
+        } else {
+          var index = 0;
+          for (index; index < countryData[0].languages.length - 1; index++) {
+            languages = languages + countryData[0].languages[index].name + ", ";
+          }
+          languages = languages + countryData[0].languages[index].name;
+        }
 
-//       var currencies = " ";
+        var currencies = " ";
 
-//       if (countryData[0].currencies.length === 1) {
-//         currencies = currencies + countryData[0].languages[0].name;
-//       } else {
-//         var index = 0;
-//         for (let index = 0; index < (countryData[0].currencies.length) - 1; index++) {
-//           currencies =
-//             currencies + countryData[0].currencies[index].name + ", ";
-//         }
-//         currencies = currencies + countryData[0].currencies[index].name;
-//       }
+        if (countryData[0].currencies.length === 1) {
+          currencies = currencies + countryData[0].languages[0].name;
+        } else {
+          var index = 0;
+          for (
+            let index = 0;
+            index < countryData[0].currencies.length - 1;
+            index++
+          ) {
+            currencies =
+              currencies + countryData[0].currencies[index].name + ", ";
+          }
+          currencies = currencies + countryData[0].currencies[index].name;
+        }
 
-//       var borders = [];
+        var borders = [];
 
-//       if (countryData[0].borders.length === 1) {
-//         borders[0] = countryData[0].borders[0];
-//       } else {
-//         var index = 0;
-//         for (let index = 0; index < (countryData[0].borders.length) - 1; index++) {
-//           borders[index] =
-//             countryData[0].borders[index];
-//         }
-//         borders[index] =
-//         countryData[0].borders[index];
-//       }
+        if (countryData[0].borders) {
+          if (countryData[0].borders.length === 1) {
+            borders[0] = countryData[0].borders[0];
+          } else {
+            var index = 0;
+            for (
+              let index = 0;
+              index < countryData[0].borders.length - 1;
+              index++
+            ) {
+              borders[index] = countryData[0].borders[index];
+            }
+            borders[index] = countryData[0].borders[index];
+          }
+        } else {
+          borders = ["None"];
+        }
 
-//       res.render("singleCountry", {
-//         country: countryData[0],
-//         currencies: currencies,
-//         languages: languages,
-//         borders: borders
+        res.render("singleCountry", {
+          country: countryData[0],
+          currencies: currencies,
+          languages: languages,
+          borders: borders,
+        });
+      });
+    });
+  }
+});
+
+// app.get("/:continent", (req, res) => {
+//   if (req.params.continent) {
+//     url = "https://restcountries.com/v3.1/region/" + req.params.continent;
+//     https.get(url, function (response) {
+//       response.on("data", (chunk) => (body += chunk));
+//       response.on("end", () => {
+//         holeInfo = JSON.parse(body);
+//         res.render("countriesList", {
+//           countries: holeInfo,
+//         });
+//         body = "";
 //       });
 //     });
-//   });
+//   }
 // });
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");
 });
-
