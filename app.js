73895861a7a153b4d905af413be0c9d01fb3d4b1
Pedro Alegre Perrotta <pedroalegre29@gmail.com>
2022-01-08 23:02:53 -0300

    ...

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 let holeInfo = [];
-let str = "";
 let body = "";
 let url = "https://restcountries.com/v3.1/all";
 
@@ -23,12 +22,18 @@ app.get("/", (req, res) => {
       body = "";
     });
   });
-})
+});
 
-app.get("/name/:search", (req, res) => {
-  console.  log(req.params.search);
-  if ((req.params.search == "europe") || (req.params.search == "africa") || (req.params.search == "america") || (req.params.search == "asia") || (req.params.search == "oceania")) {
-    url = "https://restcountries.com/v3.1/region/" + req.params.continent;
+app.get("/:search", (req, res) => {
+  let search = req.params.search.toLowerCase();
+  if (
+    search == "europe" ||
+    search == "africa" ||
+    search == "america" ||
+    search == "asia" ||
+    search == "oceania"
+  ) {
+    url = "https://restcountries.com/v3.1/region/" + req.params.search;
     https.get(url, function (response) {
       response.on("data", (chunk) => (body += chunk));
       response.on("end", () => {
@@ -36,93 +41,90 @@ app.get("/name/:search", (req, res) => {
         res.render("countriesList", {
           countries: holeInfo,
         });
+        
         body = "";
       });
     });
   } else {
-    if (req.params.search != 0) {
-      const singleCountryUrl =
-        "https://restcountries.com/v3.1/name/" + req.params.search;
-      https.get(singleCountryUrl, function (response) {
-        response.on("data", function (data) {
-          const countryData = JSON.parse(data);
-  
-          var languages = " ";
-  
-          if ((countryData[0].languages.length) === 1) {
-            languages = languages + countryData[0].languages[0].name;
-          } else {
-            var index = 0;
-            for (index; index < countryData[0].languages.length - 1; index++) {
-              languages = languages + countryData[0].languages[index] + ", ";
-            }
-            languages = languages + countryData[0].languages[index];
+    const singleCountryUrl =
+      "https://restcountries.com/v3.1/name/" + req.params.search;
+    https.get(singleCountryUrl, function (response) {
+      response.on("data", function (data) {
+        const countryData = JSON.parse(data);
+
+        var languages = " ";
+
+        if (countryData[0].languages.length === 1) {
+          languages = languages + countryData[0].languages[0].name;
+        } else {
+          var index = 0;
+          for (index; index < countryData[0].languages.length - 1; index++) {
+            languages = languages + countryData[0].languages[index] + ", ";
           }
-  
-          var currencies = " ";
-  
-          if (countryData[0].currencies.length === 1) {
-            currencies = currencies + countryData[0].languages[0].name;
+          languages = languages + countryData[0].languages[index];
+        }
+
+        var currencies = " ";
+
+        if (countryData[0].currencies.length === 1) {
+          currencies = currencies + countryData[0].languages[0].name;
+        } else {
+          var index = 0;
+          for (
+            let index = 0;
+            index < countryData[0].currencies.length - 1;
+            index++
+          ) {
+            currencies = currencies + countryData[0].currencies[index] + ", ";
+          }
+          currencies = currencies + countryData[0].currencies[index];
+        }
+
+        var borders = [];
+
+        if (countryData[0].borders) {
+          if (countryData[0].borders.length === 1) {
+            borders[0] = countryData[0].borders[0];
           } else {
             var index = 0;
             for (
               let index = 0;
-              index < countryData[0].currencies.length - 1;
+              index < countryData[0].borders.length - 1;
               index++
             ) {
-              currencies =
-                currencies + countryData[0].currencies[index] + ", ";
-            }
-            currencies = currencies + countryData[0].currencies[index];
-          }
-  
-          var borders = [];
-  
-          if (countryData[0].borders) {
-            if (countryData[0].borders.length === 1) {
-              borders[0] = countryData[0].borders[0];
-            } else {
-              var index = 0;
-              for (
-                let index = 0;
-                index < countryData[0].borders.length - 1;
-                index++
-              ) {
-                borders[index] = countryData[0].borders[index];
-              }
               borders[index] = countryData[0].borders[index];
             }
-          } else {
-            borders = ["None"];
+            borders[index] = countryData[0].borders[index];
           }
-  
-          res.render("singleCountry", {
-            country: countryData[0],
-            currencies: currencies,
-            languages: languages,
-            borders: borders,
-          });
+        } else {
+          borders = ["None"];
+        }
+
+        res.render("singleCountry", {
+          country: countryData[0],
+          currencies: currencies,
+          languages: languages,
+          borders: borders,
         });
       });
-    }
+    });
   }
-})
+});
 
-// app.get("/:continent", (req, res) => {
-//   if (req.params.continent) {
-//     url = "https://restcountries.com/v3.1/region/" + req.params.continent;
-//     https.get(url, function (response) {
-//       response.on("data", (chunk) => (body += chunk));
-//       response.on("end", () => {
-//         holeInfo = JSON.parse(body);
-//         res.render("countriesList", {
-//           countries: holeInfo,
-//         });
-//         body = "";
-//       });
-//     });
-//   }
-// });
+app.post("/name", (req, res) => {
+  console.log(req.body);
+  // url = "https://restcountries.com/v3.1/region/" + req.body.cityName;
+  // https.get(url, function (response) {
+  //   response.on("data", (chunk) => (body += chunk));
+  //   response.on("end", () => {
+  //     holeInfo = JSON.parse(body);
+  //     res.render("countriesList", {
+  //       countries: holeInfo,
+  //     });
+  //     body = "";
+  //   });
+  // });
+});
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");
